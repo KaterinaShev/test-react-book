52cb54ea0523556a28dc218530152ceb18b8d06c
Katerina Shevchenko <katyusha.shev18@gmail.com>
2023-05-17 00:59:38 +0300

    first version

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import './App.css';
+import Book from './components/book/Book';
+import { useDispatch } from 'react-redux';
+import { fetchPages } from './store/pages/PagesSlice';
+
+function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchPages())
+  })
+  return (
+    <>
+      <Book/>
+    </>
+    
+  );
+}
+
+export default App;
